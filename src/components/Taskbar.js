f62049a01a24c705a7e4f30339bd5a1c14ec3f67
Rone <Gamewarrior1andbowser@gmail.com>
2026-06-12 10:31:29 -0500

    With little success, a upcomming group project and broken Wi-fi at home. I think that I should take the F on this and get to making a (hopefully) high quallity Group Project

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,20 +1,27 @@
-import {useReducer} from 'react';
+import {useReducer, useState} from 'react';
 import Note from './Note';
 
 function Taskbar() {
-  const tasks = [{id: 0, name: "test", date: "June 11, 8:34 am", isCompleted: false}];
-  const [id, setId] = useReducer(0);
+  const tasks = [{id: 0, name: "test", 
+    date: "June 10, 8:34 am", isCompleted: false}, 
+    {id: 1, name: "end my suffering", 
+    date: "June 12, 11:14 am", isCompleted: false}];
+  const [id, setId] = useState(0);
   const [input, setInput] = useReducer('');
-  let count = 0;
+  let count = 1;
 
   const Toggle = () => {
     tasks[id].isCompleted = !tasks[id].isCompleted;
   }
   const Delete = () => {
-    tasks.pop(id)
+    tasks.pop(id);
   }
   const Save = () => {
     tasks[id].name = input;
+  }
+  const Add = () => {
+    tasks.push({id: count++, name: input, 
+      date: new Date().toLocaleTimeString, isCompleted: false});
   }
 
 
@@ -22,8 +29,9 @@ function Taskbar() {
     <>
       <section className='flex center'>
         <div className='textbar'>
-          <input type='text' placeholder='New Task'/>
-          <input type='button' value='Add'/>
+          <input type='text' placeholder='New Task' 
+          onChange={event => setInput(event.target.value)}/>
+          <input type='button' value='Add' onClick={Add}/>
         </div>
       </section>
       <section>
